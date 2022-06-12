bacf6b91e7080c65768256e6d31961793bd43075
vijaynaidu440 <vijaynaidu440@gmail.com>
2022-06-12 13:14:57 +0200

    final commit

--- src/store/store.js
+++ src/store/store.js
@@ -5,5 +5,6 @@ import details from '../reducers/details';
 import handleBus from '../reducers/handleBus'
 
 const reducers = combineReducers({ search, buses, details, handleBus });
-const store = createStore(reducers);
-export default store;
+export const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+//export default store;
